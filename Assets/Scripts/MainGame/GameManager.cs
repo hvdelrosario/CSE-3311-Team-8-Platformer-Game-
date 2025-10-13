7905d31c72ba837ff25ff1a1ec7912d0d477856c
Woohoolol <137485756+Woohoolol@users.noreply.github.com>
2025-10-13 16:26:56 -0500

    tilemap inclusion, refining signposts

--- Assets/Scripts/MainGame/GameManager.cs
+++ Assets/Scripts/MainGame/GameManager.cs
@@ -8,21 +8,23 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject temp = Instantiate(signpost, new Vector3(9, 3, 0), Quaternion.Euler(0, 0, 0));        
-        temp.GetComponent<Signpost>().dialoguePanel = dialogueBox;
-        List<string> texts = temp.GetComponent<Signpost>().texts;
-        texts.Add("Hi, how are you");
-        texts.Add("Yeah ok bye");
-        Debug.Log("GOod");
-        temp = Instantiate(signpost, new Vector3(12, 3, 0), Quaternion.Euler(0, 0, 0));
-        temp.GetComponent<Signpost>().dialoguePanel = dialogueBox;
-        texts = temp.GetComponent<Signpost>().texts;
-        texts.Add("hfdkajfdhskjafda");
+        generateSignpost(new Vector2(9, 3), new string[] {"Hi how are you", "Ok that's good"});
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void generateSignpost(Vector2 location, string[] textToInsert)
+    {
+        GameObject temp = Instantiate(signpost, location, Quaternion.Euler(0, 0, 0));        
+        temp.GetComponent<Signpost>().dialoguePanel = dialogueBox;
+        List<string> texts = temp.GetComponent<Signpost>().texts;
+        foreach(string text in textToInsert)
+        {
+            texts.Add(text);
+        }
     }
 }
